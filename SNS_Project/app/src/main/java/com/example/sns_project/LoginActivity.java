215c7c7c708434181da623f5f38fd3fc46264f3a
Minhyeok1995 <repeater2487@naver.com>
2022-04-06 18:03:09 +0900

    2022_04_06

--- SNS_Project/app/src/main/java/com/example/sns_project/LoginActivity.java
+++ SNS_Project/app/src/main/java/com/example/sns_project/LoginActivity.java
@@ -2,13 +2,11 @@ package com.example.sns_project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,70 +15,76 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "LoginActivity";
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.LoginButton).setOnClickListener(onClickListener);
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        findViewById(R.id.gotoSignupButton).setOnClickListener(onClickListener);
+        findViewById(R.id.LoginButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoPasswordResetButton).setOnClickListener(onClickListener);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-    }
-    View.OnClickListener onClickListener = new View.OnClickListener(){
+    View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
-        public void onClick(View v){
+        public void onClick(View v) {
             switch (v.getId()){
+                case R.id.gotoSignupButton:
+                    myStartActivity(SignUpActivity.class);
+                    break;
                 case R.id.LoginButton:
-                    Log.e("클릭","클릭");
-                    signUP();
+                    login();
+                    break;
+                case R.id.gotoPasswordResetButton:
+                    myStartActivity(PasswordResetActivity.class);
                     break;
             }
         }
     };
 
-    private void signUP(){
-        String email = ((EditText)findViewById(R.id.emailText)).getText().toString();
-        String password = ((EditText)findViewById(R.id.LoginpasswordEditText)).getText().toString();
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
 
-        if(email.length()>0 && password.length()>0){
+    private void login() {
+        String email = ((EditText)findViewById(R.id.emailText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0){
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                startToast("로그인 성공");
-                                startMainActivity();
+                                startToast("로그인에 성공하였습니다.");
+                                finish();
                             } else {
-                                // If sign in fails, display a message to the user.
-                                startToast(task.getException().toString());
+                                if(task.getException() != null){
+                                    startToast(task.getException().toString());
+                                }
                             }
                         }
                     });
-        }
-        else {
-            startToast("이메일 혹은 비밀번호를 입력하여주십시오.");
+        }else {
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
         }
     }
 
-    private void startToast(String msg){
+    private void startToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void startMainActivity(){
-        Intent intent = new Intent(this, MainActivity.class);
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
