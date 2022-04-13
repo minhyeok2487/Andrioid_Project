c0a3db65f804078daa2fc5ff425bfd953ab0e2a7
Minhyeok1995 <repeater2487@naver.com>
2022-04-13 18:52:10 +0900

    백그라운드 알림, 실시간 데이터 변경

--- SNS_Project/app/src/main/java/com/example/sns_project/MainActivity.java
+++ SNS_Project/app/src/main/java/com/example/sns_project/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.sns_project;
 
+import static com.google.firebase.messaging.Constants.MessagePayloadKeys.SENDER_ID;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -23,18 +26,29 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends BasicActivity {
     private static final String TAG = "MainActivity";
     TextView CurrentEmail, CurrentName, CurrentPhone;
+    TextView notificationText;
+    private static DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +67,7 @@ public class MainActivity extends BasicActivity {
                         if (document.exists()) {
                             Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                             CurrentEmail = (TextView)findViewById(R.id.EmailText);
-                            CurrentEmail.setText("접속한 이메일 : "+FirebaseAuth.getInstance().getCurrentUser().getEmail());
+                            CurrentEmail.setText("접속한 이메일 : "+user.getEmail());
                             CurrentName = (TextView) findViewById(R.id.NameText);
                             CurrentName.setText("접속한 이름 : " + document.getData().get("name"));
                             CurrentPhone = (TextView) findViewById(R.id.PNumText);
@@ -85,6 +99,7 @@ public class MainActivity extends BasicActivity {
                                         document.getData().get("publisher").toString(),
                                         new Date(document.getDate("createdAt").getTime())));
                             }
+
                             RecyclerView recyclerView = findViewById(R.id.recyclerView);
                             recyclerView.setHasFixedSize(true);
                             recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -96,6 +111,39 @@ public class MainActivity extends BasicActivity {
                         }
                     }
                 });
+
+        // 파이어베이스 실시간 데이터 다루기
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        writeNewUser(user.getUid().toString(), "몰라1", user.getEmail(), "Hello1");
+
+        // [END write_message]
+        // [START read_message]
+        // Read from the database
+
+        mDatabase.child("users").child(user.getUid().toString()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.d("데이터",dataSnapshot.getValue().toString());
+                //notificationText = (TextView) findViewById(R.id.notificationText);
+                //notificationText.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        // [END read_message]
+
 
 
 
@@ -120,8 +168,34 @@ public class MainActivity extends BasicActivity {
         }
     };
 
+
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
         startActivity(intent);
     }
+
+
+    public class User {
+        public String name;
+        public String email;
+        public String message;
+
+
+        public User(String name, String email, String message) {
+            this.name = name;
+            this.email = email;
+            this.message = message;
+        }
+        public User(){ }
+
+        public String getMessage(){
+            return message;
+        }
+    }
+    public void writeNewUser(String userId, String name, String email, String message) {
+        User user = new User(name, email, message);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+
 }
