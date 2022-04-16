5a84600a2798c0e64803f91bb2c0daa507dc69e0
Minhyeok1995 <repeater2487@naver.com>
2022-04-16 17:43:33 +0900

    실시간 위치정보 데이터 전송

--- SNS_Project/app/src/main/java/com/example/sns_project/MainActivity.java
+++ SNS_Project/app/src/main/java/com/example/sns_project/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.sns_project;
 
+import static com.example.sns_project.Action.START_LOCATION_SERVICE;
 import static com.google.firebase.messaging.Constants.MessagePayloadKeys.SENDER_ID;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -51,10 +62,22 @@ public class MainActivity extends BasicActivity {
     private static TextView CurrentEmail, CurrentName, CurrentPhone, childTextview;
     private static DatabaseReference mDatabase;
     private static String ChildName, ChildEmail;
+    private LinearLayout ButtonLayout;
+
+    private static final int REQUEST_CODE = 1;
+    Button Startbutton;
+    Button Stopbutton;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         // 회원정보가 존재하는지 확인
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -135,6 +158,36 @@ public class MainActivity extends BasicActivity {
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
         findViewById(R.id.floatingActionButton).setOnClickListener(onClickListener);
         findViewById(R.id.SendSignalButton).setOnClickListener(onClickListener);
+        findViewById(R.id.SendtoParent).setOnClickListener(onClickListener);
+
+        //자식 아이디 일때만 버튼 보이기
+        if(user.getUid().equals("be8dflcIjBg0e88Jg7JtkPd1z693")){
+            ButtonLayout = findViewById(R.id.ButtonLayout);
+            ButtonLayout.setVisibility(View.GONE);
+        }
+        Startbutton = findViewById(R.id.Startbutton);
+        Startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+                //앱에 권한이 없는 경우 이 메서드는 PackageManager.PERMISSION_DENIED를 반환하고,
+                //앱이 사용자에게 명시적으로 권한을 요청해야 합니다. 즉, IF문을 통해 이를 비교하여 권한을 받았는지 못받았는지를 가려낼 수 있습니다.
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) { //위치 권한 확인
+                    //위치 권한 요청
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                } else {
+                    startLocationService();
+                }
+            }
+        });
+        Stopbutton = findViewById(R.id.Stopbutton);
+        Stopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopLocationService();
+            }
+        });
+
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -153,10 +206,54 @@ public class MainActivity extends BasicActivity {
                     EditText ParentId = (EditText)findViewById(R.id.ParentId);
                     SendData(ParentId.getText().toString());
                     break;
-
+                case R.id.SendtoParent:
+                    //데이터 보내고 받기
+                    SendData("be8dflcIjBg0e88Jg7JtkPd1z693");
+                    break;
             }
         }
     };
+
+
+
+
+    private void startLocationService() {
+        Intent startIntent = new Intent(getApplicationContext(), LocationService.class);
+        startIntent.setAction(Action.START_LOCATION_SERVICE);
+        startService(startIntent);
+        Toast.makeText(this, "위치 업데이트 실행", Toast.LENGTH_SHORT).show();
+    }
+
+    private void stopLocationService() {
+        Intent stopIntent = new Intent(getApplicationContext(), LocationService.class);
+        stopIntent.setAction(Action.STOP_LOCATION_SERVICE);
+        startService(stopIntent);
+        Toast.makeText(this, "위치 업데이트 중지", Toast.LENGTH_SHORT).show();
+    }
+
+    private void nullAction(){
+
+    }
+
+    //grantResults는 요청에 OK를 했을 때의 정보를 갖습니다.
+    //grantResults[0]에는 PackageManager.PERMISSION_GRANTED 값을 갖습니다.
+    //만약 요청이 거절되었다면 grantResults에는 아무런 데이터도 존재하지 않게됩니다.
+    //그렇게 IF문은 권한에 대해 OK를 받았을 때 수행할 작업이 들어가며 ELSE에는 권한에 대해 CANCEL을 받았을 때의 코드를 넣어줍니다.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        startLocationService();
+                    }
+                } else {
+                    Toast.makeText(this, "Permission  denied", Toast.LENGTH_SHORT).show();
+                }
+        }
+        Toast.makeText(this, "asdafdgfdfsdsfdf", Toast.LENGTH_LONG).show();
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
 
     private void myStartActivity(Class c) {
@@ -165,7 +262,7 @@ public class MainActivity extends BasicActivity {
     }
 
 
-    public class User {
+    public static class User {
         String name;
         String email;
         String message;
